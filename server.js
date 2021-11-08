4b213ea424dca37d217e062f428dc50a5b424b1b
Trill <ty3torr3s@icloud.com>
2021-11-07 22:57:41 -0500

    finalized almost completed script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-cons express = requrie ("express"); 
+const express = require ("express"); 
 
-cons app = express();
-cons PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
